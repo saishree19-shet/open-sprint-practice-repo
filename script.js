680dde317e5dfdce9f16b0e31adffd738abf663b
Varsha <152347245+varsha-kotegar@users.noreply.github.com>
2026-03-05 12:35:40 +0530

    Remove quotes array from script.js
    Removed the quotes array from script.js.

--- script.js
+++ script.js
@@ -2,9 +2,3 @@ function showQuote() {
     document.getElementById("quote").innerText =
     "Believe in yourself!";
 }
-const quotes = [
-"Believe in yourself",
-"Never give up",
-"Dream big",
-"Stay focused"
-];
